0397f1d93b8d767398841468005d2f637857d5ef
DebBlue <bluecake097@gmail.com>
2024-04-10 18:07:22 +0530

    V2.1.0

--- suckwuw/dwm/config.def.h
+++ suckwuw/dwm/config.def.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Ubuntu Mono:size=14:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Ubuntu Mono:size=14:antialias=true:autohint=true";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12:antialias=true:autohint:true","NotoColorEmoji:size=14:antialias=true:autohint=true" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
