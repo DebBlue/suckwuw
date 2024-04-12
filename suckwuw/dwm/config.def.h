410821c728a432de6b51adbb5e854ce66b07902e
DebBlue <bluecake097@gmail.com>
2024-04-12 22:11:02 +0530

    Mami

--- suckwuw/dwm/config.def.h
+++ suckwuw/dwm/config.def.h
@@ -89,6 +89,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{ MODKEY|ShiftMask,		XK_c,	   spawn,	  SHCMD("maim -s | xclip -selection clipboard -t image/png")},	
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
